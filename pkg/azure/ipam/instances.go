f3f17e47aff01ddfe90292614c471eb67bf98a40
Hadrien Patte <hadrien.patte@datadoghq.com>
2026-05-06 16:13:57 +0200

    pkg/{aws,azure}: Use k8s `sets.Set` type for string sets
    We currently have a couple `map[string]struct{}` under `pkg/aws` and
    `pkg/azure` that are used as sets. the `k8s.io/apimachinery/pkg/util/sets`
    package provides a set abstraction with methods for common operations
    (`Has()`, `Insert()`, `Delete()`, ...). In terms of
    implementation/performance, there is no change, but this makes the code
    more readable for humans as it's immediately clear that
    `sets.New[string]()` is "a set of strings" where as for someone not
    familiar with the pattern, the `make(map[string]struct{})` "map of
    string to empty struct" can be confusing.
    
    Note: `k8s.io/apimachinery/pkg/util/sets` is already widely used
    throughout the codebase so this PR does not bring in a new dependency.
    
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- pkg/azure/ipam/instances.go
+++ pkg/azure/ipam/instances.go
@@ -7,10 +7,9 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"maps"
-	"slices"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v9"
+	"k8s.io/apimachinery/pkg/util/sets"
 
 	"github.com/cilium/cilium/pkg/ipam"
 	ipamTypes "github.com/cilium/cilium/pkg/ipam/types"
@@ -148,17 +147,16 @@ func (m *InstancesManager) resyncInstance(ctx context.Context, instanceID string
 
 // extractSubnetIDs extracts unique subnet IDs from node network interfaces
 func (m *InstancesManager) extractSubnetIDs(instances *ipamTypes.InstanceMap) []string {
-	// Use map[string]struct{} as a set for efficient deduplication (O(1) insertion, zero memory overhead)
-	subnetIDs := make(map[string]struct{})
+	subnetIDs := sets.New[string]()
 
 	instances.ForeachAddress("", func(instanceID, interfaceID, ip, poolID string, address ipamTypes.Address) error {
 		if poolID != "" {
-			subnetIDs[poolID] = struct{}{}
+			subnetIDs.Insert(poolID)
 		}
 		return nil
 	})
 
-	return slices.Collect(maps.Keys(subnetIDs))
+	return subnetIDs.UnsortedList()
 }
 
 // resyncInstances performs a full sync of all instances using three-phase strategy
